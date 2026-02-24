3e196173fa884f721bd428abd34ff28ad0fc9c26
00-kelvin <112400832+00-kelvin@users.noreply.github.com>
2026-02-24 18:22:18 -0500

    working on permulations

--- src/permulations.R
+++ src/permulations.R
@@ -1,1 +1,16 @@
 library(RERconverge)
+rerpath = find.package('RERconverge')
+
+treefile = "~/orb-selection/assets/SpeciesTree_rooted.txt"
+
+#define the root species
+root_sp = "Antrodiaetus_roretzi"
+masterTree = readTrees(treefile, max.read = 200)
+
+
+
+# #perform binary CC permulation
+# permCC = getPermsBinary(100, marineFg, sisters_marine, root_sp, mamRERw, toyTrees,
+#                         masterTree, permmode="cc")
+# #calculate permulation p-values
+# permpvalCC = permpvalcor(res,permCC)

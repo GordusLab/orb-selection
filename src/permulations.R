3328411eb1cfb9bd2a709b0a84f310c194c078fa
00-kelvin <112400832+00-kelvin@users.noreply.github.com>
2026-03-19 15:03:46 -0400

    permulations script

--- src/permulations.R
+++ src/permulations.R
@@ -1,16 +1,38 @@
 library(RERconverge)
-rerpath = find.package('RERconverge')
+library(phangorn)
 
-treefile = "~/orb-selection/assets/SpeciesTree_rooted.txt"
+rerpath <- find.package('RERconverge')
+
+treefile <- "~/orb-selection/assets/SpeciesTree_full_brlen.nwk"
 
 #define the root species
-root_sp = "Antrodiaetus_roretzi"
-masterTree = readTrees(treefile, max.read = 200)
+root <- "Drosophila_melanogaster"
 
+#read in the species tree
+speciesTree <- read.tree(treefile)
+# speciesTree <- drop.tip(
+#   speciesTree, 
+#   c("Antrodiaetus_roretzi", "Orchestina_okitsui", "Falcileptoneta_japonica", "Masirana_silvicola")
+# )
 
+#make empty list - don't need gene trees for permulation
+testTrees <- list()
 
-# #perform binary CC permulation
-# permCC = getPermsBinary(100, marineFg, sisters_marine, root_sp, mamRERw, toyTrees,
-#                         masterTree, permmode="cc")
-# #calculate permulation p-values
-# permpvalCC = permpvalcor(res,permCC)
+#add species tree to "list" of trees
+testTrees$masterTree<-speciesTree 
+
+#vector of foreground tips (orb-weavers)
+orbweavers <- readLines("~/orb-selection/assets/orbweavers-list.txt")
+allspecs <- testTrees$masterTree$tip.label
+notN5 <- c("Drosophila_melanogaster", "Antrodiaetus_roretzi", "Orchestina_okitsui", "Falcileptoneta_japonica", "Masirana_silvicola")
+N5 <- allspecs[!allspecs %in% notN5]
+phenvec <- ifelse(N5 %in% orbweavers, 2, 1)
+names(phenvec) <- N5
+
+testCatPerm10000<-categoricalPermulations(
+  testTrees, 
+  phenvec, 
+  rm="ER", 
+  rp="auto", 
+  ntrees=10000
+  )

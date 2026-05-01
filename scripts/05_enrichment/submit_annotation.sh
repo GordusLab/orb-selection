77a379beb0f1337286e51f2ea01d31012f0a63c9
rockfish <crunnel2@jh.edu>
2026-05-01 16:12:31 -0400

    slurm script for annotations, but i ended up doing it locally i think

--- scripts/05_enrichment/submit_annotation.sh
+++ scripts/05_enrichment/submit_annotation.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-#SBATCH --job-name=annotate_hogs
-#SBATCH --partition=defq
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --job-name=260423_annotate_hogs
+#SBATCH --partition=parallel
+#SBATCH --account=agordus1
 #SBATCH --mem=4G
-#SBATCH --time=01:00:00
-#SBATCH --array=1-10000%100 # Adjust array size to number of HOGs and limit parallel jobs
+#SBATCH --time=00:05:00
+#SBATCH --array=1-10 
+#SBATCH --output=/data/agordus1/crunnel2/reports/%x/%A_%a.out
+
+
+module load anaconda
+conda activate /home/crunnel2/anaconda3/envs/selection
+module load Biopython/1.81-foss-2022b
+module load py-numpy/1.24.4
+
+#make directory to store slurm reports
+mkdir -p /data/agordus1/crunnel2/reports/$SBATCH_JOB_NAME/
 
 # Define paths
-HOG_LIST_FILE="data/N5_hogs_min_occ_30.txt"
-N0_TSV="data/N5.tsv"
-ORTHO_DIR="data/orthofinder/Results_Apr16/Orthogroup_Sequences"
-REF_DB="data/blast_db/dmel_db"
-OUTPUT_DIR="results/annotated_hogs"
-SCRIPT_PATH="scripts/05_enrichment/annotate_one_hog.py"
+HOG_LIST_FILE="/home/crunnel2/orb-selection/data/N5_hogs_min_occ_30.txt"
+N0_TSV="/home/crunnel2/orb-selection/data/N5.tsv"
+ORTHO_DIR="/scratch4/agordus1/crunnel2/orthofinder_fastas"
+REF_DB="/scratch4/agordus1/crunnel2/GCF_043381705.1/cds_from_genomic.fna"
+OUTPUT_DIR="/scratch4/agordus1/crunnel2/annotated_hogs"
+SCRIPT_PATH="/home/crunnel2/orb-selection/scripts/05_enrichment/annotate_one_hog.py"
 
 # Get the HOG for the current array task
 HOG=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$HOG_LIST_FILE")

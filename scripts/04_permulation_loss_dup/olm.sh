a6e2a9f6f76d0be1fe0dbdba4e3e457a16405c82
rockfish <crunnel2@jh.edu>
2026-05-04 13:32:01 -0400

    rockfish edits

--- scripts/04_permulation_loss_dup/olm.sh
+++ scripts/04_permulation_loss_dup/olm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=260501_phyloglm
+#SBATCH --job-name=260504_phyloglm
 #SBATCH --partition=parallel
 #SBATCH --account=agordus1
 #SBATCH --nodes=1
@@ -7,9 +7,12 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --time=24:00:00
 #SBATCH --mem=0
-#SBATCH --mailtype=ALL
+#SBATCH --mail-type=ALL
 #SBATCH --output=/data/agordus1/crunnel2/reports/%x/%A_%a.out
 #SBATCH --error=/data/agordus1/crunnel2/reports/%x/%A_%a.err
+
+# module load R/4.5.1-gfbf-2023b
+export R_LIBS_USER=/home/crunnel2/R/x86_64-pc-linux-gnu-library/4.5
 
 #make directory to store slurm reports
 mkdir -p /data/agordus1/crunnel2/reports/${SBATCH_JOB_NAME}/
@@ -20,4 +23,4 @@ module load R
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 
-Rscript /path/to/orb-selection/scripts/04_permulation_loss_dup/olm.R
+Rscript /home/crunnel2/orb-selection/scripts/04_permulation_loss_dup/olm.R

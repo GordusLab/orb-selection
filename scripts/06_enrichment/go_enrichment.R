127664357b56a5979cbb5b4fbf8e95e25a1f0290
00-kelvin <112400832+00-kelvin@users.noreply.github.com>
2026-08-13 10:40:49 -0400

    rerun go enrichment for copy number results

--- scripts/06_enrichment/go_enrichment.R
+++ scripts/06_enrichment/go_enrichment.R
@@ -9,8 +9,8 @@
 library(here)
 library(topGO)
 
-folders <- dir(here("results/significant_gene_id_lists/copy_number/udiv"), full.names = TRUE)
-annot_filename <- here("data/udiv_go_annots.all.tsv")
+folders <- dir(here("results/significant_gene_id_lists/copy_number/ptep"), full.names = TRUE)
+annot_filename <- here("data/ptep_go_annots.all.tsv")
 
 for (folder in folders) {
   # hit files are all files without "universe" in the name

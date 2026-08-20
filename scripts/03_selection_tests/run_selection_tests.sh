dbc86af434d45ae9411310c1f6915961fb8b4eef
rockfish <crunnel2@jh.edu>
2026-08-20 13:16:40 -0400

    new hyphy version

--- scripts/03_selection_tests/run_selection_tests.sh
+++ scripts/03_selection_tests/run_selection_tests.sh
@@ -2,14 +2,14 @@
 
 # Runs RELAX and BUSTED-PH (fw and rev) for one HOG per SLURM array task.
 
-#SBATCH --job-name=260810_run_selection_tests_12cpus
+#SBATCH --job-name=260820_relax_fix_fg
 #SBATCH --partition=parallel
 #SBATCH --account=agordus1
-#SBATCH --time=3-00:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --mail-user=crunnel2@jhu.edu
 #SBATCH --mail-type=ALL
-#SBATCH --array=33,482,958,113,1011,1190,1316,3205,1354,2456,860
-#SBATCH -n 12
+#SBATCH --array=1-6
+#SBATCH -n 2
 #SBATCH --output=/data/agordus1/crunnel2/reports/%x/%A_%a.out
 #SBATCH --error=/data/agordus1/crunnel2/reports/%x/%A_%a.err
 
@@ -17,17 +17,17 @@
 mkdir -p /data/agordus1/crunnel2/reports/$SBATCH_JOB_NAME/
 
 module load anaconda
-conda activate /home/crunnel2/anaconda3/envs/selection
+conda activate /home/crunnel2/anaconda3/envs/hyphy-new
 
 WD=/scratch4/agordus1/crunnel2/hyphy_wd
 HOG_LIST=/home/crunnel2/orb-selection/data/N5.udiv.o75_list.txt
 
-HYPHY_ANALYSES_DIR=/home/crunnel2/bin/hyphy-analyses/
+#HYPHY_ANALYSES_DIR=/home/crunnel2/bin/hyphy-analyses/
 
 CURRENT_HOG=$(sed "${SLURM_ARRAY_TASK_ID}q;d" $HOG_LIST)
 
 ## RELAX
-RELAX_OUT=${WD}/${CURRENT_HOG}_RELAX.json
+RELAX_OUT=${WD}/${CURRENT_HOG}_RELAX2.json
 #check if RELAX has already completed for this HOG 
 if grep -q "p-value" ${RELAX_OUT}; then
 	echo "RELAX already complete."
@@ -35,8 +35,8 @@ else
 	#run relax
 	hyphy relax \
 	 CPU=${SLURM_NTASKS} \
-	 --alignment ${WD}/${CURRENT_HOG}.orb_fg.nex \
-	 --test "Unlabeled branches" \
+	 --alignment ${WD}/${CURRENT_HOG}.non_orb_fg.nex \
+	 --test "FOREGROUND" \
 	 --multiple-hits Double+Triple \
 	 --models Minimal \
 	 --srv Yes \
@@ -52,7 +52,7 @@ if grep -q "p-value" ${BUSTEDPH_ORB_OUT}; then
 	echo "BUSTED-PH-fw already complete."
 else
 	#run busted-ph
-	hyphy "$HYPHY_ANALYSES_DIR/BUSTED-PH/BUSTED-PH.bf" \
+	hyphy busted-ph \
 	 CPU=${SLURM_NTASKS} \
 	 --alignment ${WD}/${CURRENT_HOG}.orb_fg.nex \
 	 --branches FOREGROUND \
@@ -66,7 +66,7 @@ if grep -q "p-value" ${BUSTEDPH_NON_ORB_OUT}; then
 	echo "BUSTED-PH-rev already complete."
 else
 	#run busted-ph
-	hyphy "$HYPHY_ANALYSES_DIR/BUSTED-PH/BUSTED-PH.bf" \
+	hyphy busted-ph \
 	 CPU=${SLURM_NTASKS} \
 	 --alignment ${WD}/${CURRENT_HOG}.non_orb_fg.nex \
 	 --branches FOREGROUND \
